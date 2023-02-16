8322ad025ee605ce7fde144fd25672fe54392068
WBomfim <willianbomfim@hotmail.com>
2023-02-16 16:41:16 -0300

    feat: Implementado o método formatAccountNumber na classe AccountNumberFormatter.

--- src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
@@ -1,5 +1,6 @@
 package com.betrybe.acc.java.bankaccount;
 
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * AccountNumberFormatter.
@@ -12,7 +13,18 @@ public class AccountNumberFormatter {
    * @param literalAccountNumber número da conta
    */
   public String formatAccountNumber(int literalAccountNumber) {
+    byte SIZE_ACCOUNT_NUMBER = 6;
+    String accountNumber = String.valueOf(literalAccountNumber);
+  
+    if (accountNumber.length() < SIZE_ACCOUNT_NUMBER) {
+      return StringUtils.leftPad(accountNumber, SIZE_ACCOUNT_NUMBER, "0");
+    }
 
+    if (accountNumber.length() > SIZE_ACCOUNT_NUMBER) {
+      return StringUtils.right(accountNumber, SIZE_ACCOUNT_NUMBER);
+    }
+
+    return accountNumber;
   }
 
 }
