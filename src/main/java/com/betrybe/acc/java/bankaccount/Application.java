2af449b89d31ea28a1768448fff32f06ccf0e6f9
WBomfim <willianbomfim@hotmail.com>
2023-02-16 17:35:48 -0300

    feat: Implementado o método main na classe Application - O método é responsável por coletar o input do usuário e retornar uma mensagem com o número da conta formatado

--- src/main/java/com/betrybe/acc/java/bankaccount/Application.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/Application.java
@@ -1,5 +1,7 @@
 package com.betrybe.acc.java.bankaccount;
 
+import java.util.Scanner;
+
 /**
  * A class.
  */
@@ -9,6 +11,21 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    AccountNumberFormatter formatter = new AccountNumberFormatter();
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Entre com o numero da conta: ");
+
+    try {
+      int accountNumber = scan.nextInt();
+      String formattedAccountNumber = formatter.formatAccountNumber(accountNumber);
+
+      System.out.println("Numero da conta: " + formattedAccountNumber);
+    } catch (Exception e) {
+      System.out.println("O número da conta deve conter apenas números.");
+    } finally {
+      scan.close();
+    }
 
   }
 
